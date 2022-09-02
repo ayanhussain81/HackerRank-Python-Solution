b6bdf79b475c4ebeb0e48cdf42c4bc0d4ffde869
Ayan Hussain <ayanhussain746@gmail.com>
2022-09-02 12:38:35 +0500

    Updated

--- Collections/Counters.py
+++ Collections/Counters.py
@@ -18,3 +18,19 @@ for i in range(numCust):
         pass
 print(ans)
 # END
+#                                   OR
+
+import collections
+
+numShoes = int(input())
+shoes = collections.Counter(map(int, input().split()))
+numCust = int(input())
+income = 0
+
+for i in range(numCust):
+    size, price = map(int, input().split())
+    if shoes[size]: 
+        income += price
+        shoes[size] -= 1
+
+print(income)
