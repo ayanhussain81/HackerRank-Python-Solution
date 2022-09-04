171d352def8e8ab8a382ace3765cf43e6ce45d8a
Ayan Hussain <ayanhussain746@gmail.com>
2022-09-04 15:56:46 +0500

    Updated

---
+++ Sets/IntroductionToSets.py
@@ -0,0 +1,16 @@
+# Introduction's to set in Python - Hacker Rank Solution
+# START
+def average(array):
+    x = sum(set(array))
+    z= len(set(array))
+    ans = x/z
+    return ans
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
+# END
